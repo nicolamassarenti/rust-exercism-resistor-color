684a492f7fc417ddab504851bc4d3a6264925ab0
Nicola Massarenti <nicola.massarenti@gmail.com>
2023-02-19 01:34:55 +0100

    WIP

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+use int_enum::IntEnum;
+
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, IntEnum)]
+pub enum ResistorColor {
+    Black=0,
+    Brown=1,
+    Red=2,
+    Orange=3,
+    Yellow=4,
+    Green=5,
+    Blue=6,
+    Violet=7,
+    Grey=8,
+    White=9,
+}
+
+pub fn color_to_value(_color: ResistorColor) -> u32 {
+    _color as u32
+}
+
+pub fn value_to_color_string(value: u32) -> String {
+    unimplemented!(
+        "convert the value {} into a string representation of color",
+        value
+    )
+
+}
+
+pub fn colors() -> Vec<ResistorColor> {
+    unimplemented!("return a list of all the colors ordered by resistance")
+}
